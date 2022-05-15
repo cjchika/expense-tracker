d120ad99d61f0776180d082155f3ea726b8d0f09
Chijioke Chika <8pluscreative@gmail.com>
2022-05-15 09:22:58 -0400

    Update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-	const expenseDate = new Date(2022, 4, 15)
-	const expenseTitle = 'House Rent';
-	const expenseAmount = 500.99;
-
+	
   return ( 
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div> 
 	);
