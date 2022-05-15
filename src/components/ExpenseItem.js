d4eb3d75016a6ee4d9ee14104a1a3091d4e328ab
Chijioke Chika <8pluscreative@gmail.com>
2022-05-15 08:28:45 -0400

    Update

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return ( 
+    <div>
+      <div>15 March 2022</div>
+      <div>
+        <h2>House Rent</h2>
+        <div>$500.50</div>
+      </div>
+    </div> 
+	);
+}
+
+export default ExpenseItem;
