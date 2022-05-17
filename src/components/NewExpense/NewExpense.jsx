b1ff12080f1e47b10046fcf47d0b2b217a717ff2
Chijioke Chika <8pluscreative@gmail.com>
2022-05-17 05:52:45 -0400

    Update

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 import './NewExpense.css';
 
 const NewExpense = () => {
 
-    return <div className="new-expense">
-        
-    </div>
+	return ( 
+		<div className="new-expense">
+			<ExpenseForm />
+		</div>
+	);	
 };
 
 export default NewExpense;
