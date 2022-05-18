da69105abcf5c795e1aa2a490463036146cd282a
Chijioke Chika <8pluscreative@gmail.com>
2022-05-18 05:15:07 -0400

    Updated

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+	const [isEditing, setIsEditing] = useState(false);
+
 	const keepExpenseDataHandler = (enteredExpenseData) => {
 		const expenseData = {
 			...enteredExpenseData, 
 			id: Math.random().toString()
 		};
 		props.onAddExpense(expenseData)
+		setIsEditing(false);
 	};
+
+	const startEditingHandler = () => {
+		setIsEditing(true)
+	};
+
+	const stopEditingHandler = () => {
+		setIsEditing(false)
+	}
 
 	return ( 
 		<div className="new-expense">
-			<ExpenseForm onKeepExpenseDate={keepExpenseDataHandler}/>
+			{ !isEditing && <button onClick={startEditingHandler}>Add New Expenses</button>}
+			{isEditing && <ExpenseForm onKeepExpenseDate={keepExpenseDataHandler} onCancel={stopEditingHandler}/>}
 		</div>
 	);	
 };
